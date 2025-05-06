a149136444bf58068d9367df7340c85754b5e104
Wasid786 <114327679+Wasid786@users.noreply.github.com>
2025-05-06 12:31:00 +0530

    signup ui added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,11 +6,29 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/", app.homeHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/signup", app.signupHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/signin", app.signinHandler)
-	return router
+
+	return enableCORS(router)
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Allow from your frontend origin
+		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Handle preflight OPTIONS request
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
